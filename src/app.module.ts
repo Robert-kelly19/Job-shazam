0d6d74b1beeab15d73a7cb30808cadbffcd1f0d7
akuma19 <kellyhandy31@gmail.com>
2025-06-23 14:25:49 +0100

    feat(entity file): added entity file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import {Module} from "@nestjs/common";
-import {AppController} from "./app.controller";
-import {AppService} from "./app.service";
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: "postgres",
+        host: config.get<string>("DB_HOST"),
+        user: config.get<string>("DB_USER"),
+        password: config.get<string>("DB_PASSWORD"),
+        name: config.get<string>("DB_NAME"),
+        port: config.get<number>("DB_PORT"),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+  ],
 })
 export class AppModule {}
