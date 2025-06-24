16674a1c78add91a419fa420dea1430d21bc6281
akuma19 <kellyhandy31@gmail.com>
2025-06-24 13:31:54 +0100

    feat(db connection): connected to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import {Module} from "@nestjs/common";
 import {ConfigModule, ConfigService} from "@nestjs/config";
 import {TypeOrmModule} from "@nestjs/typeorm";
+import {AppController} from "./app.controller";
+import {AppService} from "./app.service";
 
 @Module({
   imports: [
@@ -10,17 +12,27 @@ import {TypeOrmModule} from "@nestjs/typeorm";
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: "postgres",
-        host: config.get<string>("DB_HOST"),
-        username: config.get<string>("DB_USER"),
-        password: config.get<string>("DB_PASSWORD"),
-        database: config.get<string>("DB_NAME"),
-        port: config.get<number>("DB_PORT"),
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
+      useFactory: (config: ConfigService) => {
+        const envFile = ["DB_HOST", "DB_USER", "DB_PASSWORD", "DB_NAME", "DB_PORT", "NODE_ENV"];
+        for (const envVar of envFile) {
+          if (!config.get(envVar)) {
+            throw new Error(`environment variables are missing check your .env file:${envVar}`);
+          }
+        }
+        return {
+          type: "postgres",
+          host: config.get<string>("DB_HOST"),
+          username: config.get<string>("DB_USER"),
+          password: config.get<string>("DB_PASSWORD"),
+          database: config.get<string>("DB_NAME"),
+          port: config.get<number>("DB_PORT"),
+          autoLoadEntities: true,
+          synchronize: config.get<string>("NODE_ENV") !== "production",
+        };
+      },
     }),
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
