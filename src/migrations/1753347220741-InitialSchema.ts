bc568355cd54b3b7993e800e892d24928c54e08d
akuma19 <kellyhandy31@gmail.com>
2025-07-24 09:54:54 +0100

    feat(migration): typeOrm migration

---
+++ src/migrations/1753347220741-InitialSchema.ts
@@ -0,0 +1,19 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class InitialSchema1753347220741 implements MigrationInterface {
+  name = "InitialSchema1753347220741";
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "job" DROP CONSTRAINT "FK_308fb0752c2ea332cb79f52ceaa"`);
+    await queryRunner.query(
+      `ALTER TABLE "job" ADD CONSTRAINT "FK_308fb0752c2ea332cb79f52ceaa" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "job" DROP CONSTRAINT "FK_308fb0752c2ea332cb79f52ceaa"`);
+    await queryRunner.query(
+      `ALTER TABLE "job" ADD CONSTRAINT "FK_308fb0752c2ea332cb79f52ceaa" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+  }
+}
