521c58a06069ab2fcfbc3a1c9a582f4897597978
akuma19 <kellyhandy31@gmail.com>
2025-07-24 13:23:34 +0100

    feat(migration): typeOrm migration

--- job/job.entity.ts
+++ job/job.entity.ts
@@ -4,7 +4,7 @@ import { Type} from "class-transformer";
 import { User } from "../user/user.entity";
 import { SavedJob } from "../saved-job/saved-job.entity";
 
-@Entity('jobs')
+@Entity('job')
 export class Job {
     @PrimaryGeneratedColumn('uuid')
     id: string;
